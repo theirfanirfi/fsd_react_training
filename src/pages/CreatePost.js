93cf43f9f2aeb5f06cc688863d56fce58d0c92a7
zksikhan <zksikhan1@gmail.com>
2025-05-25 18:03:52 +0500

    create-post form

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,6 +1,20 @@
 const CreatePost = () => {
     return (
         <>
+        <div>
+            <h1>Create Post</h1>
+            <form>
+                <div>
+                    <label for="title">Title:</label>
+                    <input type="text" id="title" name="title" required />
+                </div>
+                <div>
+                    <label for="body">Body:</label>
+                    <textarea id="content" name="body" required></textarea>
+                </div>
+                <button type="submit">Create Post</button>
+            </form>
+        </div>
         <h1>Create Post</h1>
         <h2> Form</h2>
         </>
