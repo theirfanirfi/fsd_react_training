f11a237b84ad427fde2933a20522e27a0071babf
RajaRamish <ramish1342@gmail.com>
2025-05-25 18:27:33 +0500

    Create Form

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,10 +1,50 @@
-const CreatePost = () => {
-    return (
-        <>
+import {usestate} from "react";
+
+const CreatePost = () => 
+{
+    const[title, setTitle] = useState("");
+    const[content, setContent] = useState("");
+
+    const handlesubmit = (e) =>
+    {
+        e.preventDefault();
+
+        console.log("Post Submitted: ", {title, content});
+
+        setTitle("");
+        setContent("");
+    }
+
+return
+(
+    <>
         <h1>Create Post</h1>
-        <h2> Form</h2>
-        </>
-    )
-}
+        <form onSubmit={handleSubmit}>
+            <div>
+                <lable htmlFor="title">Title</lable><br />
+                <input
+                    type="text"
+                    id="title"
+                    value={title}
+                    onChange={(e) =>setTitle(e.target.value)}
+                    required
+                />
+            </div>
+            <br />
+            <div>
+                <lable htmlFor="content">Content</lable><br />
+                <textarea
+                    id="content"
+                    value={content}
+                    onChange={(e) =>setContent(e.target.value)}
+                    required
+                />
+            </div>
+            <br />
+            <button type="submit">Submit</button> 
+        </form>
+    </>
+   );
+};
 
 export default CreatePost;
